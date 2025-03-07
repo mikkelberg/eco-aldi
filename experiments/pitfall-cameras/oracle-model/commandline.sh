a0c567cd5087a4ff3183cf612a1e2f27c0af3a81
dangerstone <stinnadanger@gmail.com>
2025-03-07 10:19:53 +0100

    set up for pitfall-cameras oracle training

--- experiments/pitfall-cameras/oracle-model/commandline.sh
+++ experiments/pitfall-cameras/oracle-model/commandline.sh
@@ -3,7 +3,7 @@
 # Point to your dataset folder containing both images and COCO formatted annotations here.
 # Link name MUST be 'datasets' to be compatible with ALDI
 ln -sfT /mnt/data0/martez/images/ datasets
-ln -sfT ~/eco-aldi/annotations/ annotations
+#ln -sfT ~/eco-aldi/annotations/ annotations
 
 # Run experiment
 python ../../../aldi/tools/train_net.py --config input/OracleT-RCNN-FPN-Cityscapes_strongaug_ema_corrected.yaml --num-gpus 4
