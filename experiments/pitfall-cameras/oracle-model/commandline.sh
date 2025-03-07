e8bcc45de48941893265a2a3ea7a8234b2bef993
dangerstone <stinnadanger@gmail.com>
2025-03-07 10:24:39 +0100

    changed file structure to better specify this specific training

--- experiments/pitfall-cameras/oracle-model/commandline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Point to your dataset folder containing both images and COCO formatted annotations here.
-# Link name MUST be 'datasets' to be compatible with ALDI
-ln -sfT /mnt/data0/martez/images/ datasets
-#ln -sfT ~/eco-aldi/annotations/ annotations
-
-# Run experiment
-python ../../../aldi/tools/train_net.py --config input/pitfall-cameras_RCNN-FPN_oracle.yaml --num-gpus 4
