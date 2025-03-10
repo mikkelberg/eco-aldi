4fe96333be53b5c29aab80d10902330291b1a528
dangerstone <stinnadanger@gmail.com>
2025-03-10 10:35:20 +0100

    ran the training and evaluated on test data

--- experiments/pitfall-cameras_oracle-model/negratio40_basicgrouping_noclassbalancing/commandline.sh
+++ experiments/pitfall-cameras_oracle-model/negratio40_basicgrouping_noclassbalancing/commandline.sh
@@ -6,3 +6,6 @@ ln -sfT /mnt/data0/martez/ datasets
 
 # Run experiment
 python ../../../aldi/tools/train_net.py --config input/pitfall-cameras_RCNN-FPN_oracle.yaml --num-gpus 4
+
+# Evaluate on test-data:
+# python ../../../aldi/tools/train_net.py --eval-only --num-gpus 4 --config-file input/pitfall-cameras_RCNN-FPN_oracle.yaml MODEL.WEIGHTS output/training/pitfall-cameras_val_model_best.pth DATASETS.TEST '("pitfall-cameras_test",)'
