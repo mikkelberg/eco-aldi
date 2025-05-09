6912f02a51381a940d61a59438faf3ac556dc317
dangerstone <stinnadanger@gmail.com>
2025-05-09 11:08:43 +0200

    reindexed categories

--- scripts/dataset_prep/field_daod.py
+++ scripts/dataset_prep/field_daod.py
@@ -1,4 +1,5 @@
 import sys
+import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from utils import utils
 from utils import coco
@@ -21,25 +22,38 @@ def remove_categories(src_coco):
     src_coco["images"] = images
     src_coco["annotations"] = anns
 
-
+def reindex_categories(src_coco):
+    old_to_new_id = {}
+    
+    src_cat_id_to_name = coco.category_id_to_name(src_coco["categories"])
+    new_cat_name_to_id = coco.category_name_to_id(new_classes)
+    
+    for ann in src_coco["annotations"]:
+        name = src_cat_id_to_name[ann["category_id"]]
+        new_id = new_cat_name_to_id[name]
+        ann["category_id"] = new_id
+    
+    src_coco["categories"] = new_classes
 
 classes_to_keep = ["aranae","carabidae","diptera-hymenoptera","isopoda","myriapoda","staphylinidae"]
+new_classes = []
+for i,clss in enumerate(classes_to_keep):
+    new_classes.append({"id": i, "name": clss})
 
-src_dir = "osr-fields/"
+src_dir = "annotations/osr-fields/"
 lg_coco_file = src_dir + "lg/LG_OSR.json"
 gh_coco_file = src_dir + "gh/GH_OSR.json"
 lg_coco = coco.load_from_file(lg_coco_file)
 gh_coco = coco.load_from_file(gh_coco_file)
-
 
 # Remove the unneeded categories
 remove_categories(lg_coco)
 remove_categories(gh_coco)
 
 # Reindex remaining categories
+reindex_categories(lg_coco)
+reindex_categories(gh_coco)
 
-
-
-save_to_file(json_obj=lg_coco, path=lg_coco_file)
-save_to_file(json_obj=gh_coco, path=gh_coco_file)
+coco.save_to_file(json_obj=lg_coco, path=lg_coco_file)
+coco.save_to_file(json_obj=gh_coco, path=gh_coco_file)
 
